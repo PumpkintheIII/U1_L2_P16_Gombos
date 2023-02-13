ccc28424a6add4038acac7415f2d86b98486d201
mgombos <50267286+PumpkintheIII@users.noreply.github.com>
2023-02-13 13:20:25 +0000

    Iteration 2:

--- .ccls-cache/@home@runner@U1L2P16Gombos/main.c
+++ .ccls-cache/@home@runner@U1L2P16Gombos/main.c
@@ -8,6 +8,8 @@
 int main(void) { //define main function
   int number; //define variable as integer
 
+  printf("This program allows the user to input a whole number and have the program output the character associated with it on the ASCII Chart. You will get a prompt to enter your number:\n\t- Number: Enter a number here\n"); //instructions
+  
   printf("Number: "); //ask user for number
   scanf("%d", &number); //accept input and format as integer
 
