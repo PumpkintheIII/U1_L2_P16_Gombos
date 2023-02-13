74b829d891cb14fc9cd33ca7b9f11194150769a3
mgombos <50267286+PumpkintheIII@users.noreply.github.com>
2023-02-13 13:15:27 +0000

    Iteration 1

--- main.c
+++ main.c
@@ -3,9 +3,17 @@
   Character from ASCII Chart
   Max Gombos
 */
-#include <stdio.h>
+#include <stdio.h> //import input output functions
 
-int main(void) {
+int main(void) { //define main function
+  int number; //define variable as integer
+
+  printf("Number: "); //ask user for number
+  scanf("%d", &number); //accept input and format as integer
+
+  //print output:
+  printf("Character: ");
+  putchar(number);
   
   return 0;
 }
